5286d20e192ddae4af649778045cf1ee2d849034
Daisy Le <daisy.le0203@gmail.com>
2022-11-25 00:28:49 -0600

    added more info about projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Web Designer"],
+        strings: ["Web Developer.", "Problem Solver."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Web Designer"],
+        strings: ["Web Developer.", "Problem Solver."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true

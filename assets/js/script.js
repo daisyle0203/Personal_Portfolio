39e474699196077b5efa3c6af7380c5d36655ee0
Daisy <daisy.le0203@gmail.com>
2023-07-04 14:38:34 -0500

    add comments for readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,39 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+        // sticky navbar on scroll script: if the scroll is greater than 20px add sticky class to the navbar else remove it
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+        // scroll-up button show/hide script: if the scroll is greater than 500px add show class to the scroll-up button else remove it
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    // slide-up script
+
+    // slide-up script for smooth scroll to top: if the scroll-up button is clicked, scroll to the top of the document
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
+
+    // toggle menu/navbar script for smooth scroll: if the menu icon is clicked, add active class to the menu and toggle the menu icon
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
-    // toggle menu/navbar script
+
+    // toggle menu/navbar script for smooth scroll: toggle menu/navbar on click
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Web Developer.", "Problem Solver."],
@@ -42,6 +47,7 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
+    
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
